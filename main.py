dab135a5bda6496ee8dbdbd1f7a870c65bdd7919
kieter <balisnom@ualberta.ca>
2019-01-08 09:15:22 -0700

    lab after the script that downloads itself. I also forgot the requirements.txt file.

--- main.py
+++ main.py
@@ -7,3 +7,6 @@ print(requests.__version__)
 r = requests.get("http://www.google.com")
 print(r.status_code)
 print(r.headers)
+
+this_file = requests.get("https://raw.githubusercontent.com/kieter/cmput401lab1/master/main.py")
+print(this_file.text)
